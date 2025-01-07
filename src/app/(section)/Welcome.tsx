d7df74599a27fc7ad6af80ee57c068d8aa01dbe2
jibinshan <jibinshanu23@gmail.com>
2025-01-07 17:43:24 +0530

    changes in welcome section

--- src/app/(section)/Welcome.tsx
+++ src/app/(section)/Welcome.tsx
@@ -111,55 +111,55 @@ const WelcomeRes = ({ }) => {
                 <div className="absolute w-full h-full left-0 top-0 py-12 md:py-24">
                     <div className="h-full w-full"
                         style={{
-                            backgroundImage: "url('/images/home/about/topleaf.png') ,url('/images/home/about/bottomleaf.png')",
+                            backgroundImage: "url('/images/home/about/top.svg') ,url('/images/home/about/bottom.svg')",
                             backgroundPosition: "top left , bottom right",
-                            backgroundSize: "10% ,10%",
+                            backgroundSize: "15% ,15%",
                             backgroundRepeat: "no-repeat, no-repeat"
                         }}
                     />
                 </div>
-                <div className="z-30 flex flex-col gap-5 lg:gap-9 h-full w-full max-w-[1300px] items-center justify-center">
-                    <div className="px-5 py-6 rounded-full flex gap-2 items-center justify-center">
+                <div className="z-30 flex flex-col gap-5 lg:gap-9 h-full w-full md:px-[120px] items-center justify-center">
+                    <div className="px-5 py-6 rounded-full flex gap-4 items-center justify-center">
                         <Image
                             src="/images/home/about/delivery.png"
                             width={60}
                             height={39}
                             alt="welcome"
-                            className="w-8"
+                            className=""
                         />
-                        <div className="font-cormorant font-[400] text-base md:text-xl text-black uppercase">Now delivering with{' '}<Link href='/' className="font-cormorant text-primary-foreground border-b-[1px] border-b-primary-foreground">UberEats!</Link></div>
+                        <div className="font-cormorant font-[400] text-base md:text-3xl text-black uppercase">Now delivering with{' '}<Link href='/' className="font-cormorant text-primary-foreground border-b-[1px] border-b-primary-foreground">UberEats!</Link></div>
                     </div>
 
                     <div className="relative w-full flex justify-center items-center">
                         <div className="absolute w-full top-16 md:top-40 flex justify-center items-start">
-                            <p className="text-3xl md:text-7xl font-cormorant text-center font-[400] uppercase floating"><span className="md:text-primary-foreground">U</span>pscale Mexica<span className="md:text-primary-foreground">n</span><br /> <span className="md:text-primary-foreground">D</span>ining in Watfor<span className="md:text-primary-foreground">d</span></p>
+                            <p className="text-3xl md:text-8xl font-cormorant text-center font-[400] uppercase floating"><span className="md:text-primary-foreground">U</span>pscale Mexica<span className="md:text-primary-foreground">n</span><br /> <span className="md:text-primary-foreground">D</span>ining in Watfor<span className="md:text-primary-foreground">d</span></p>
                         </div>
-                        <div className="absolute w-full bottom-6 md:bottom-24 flex flex-col justify-center items-center gap-4 md:gap-10 py-4">
+                        <div className="absolute w-full bottom-6 md:bottom-24 flex flex-col justify-center items-center gap-4 md:gap-14 py-4">
                             <Image
                                 src="/images/home/about/flower.png"
                                 width={93}
                                 height={91}
                                 alt="welcome"
-                                className="max-w-16"
+                                className="w-16 md:max-w-full"
                             />
-                            <p className="text-lg md:text-2xl font-cormorant text-center text-[#ECE6D6] -ml-3 md:ml-0 md:max-w-[600px] font-[400]">Welcome to Jardín, an upscale Mexican dining destination where the vibrant flavors of the jungles in Southern Mexico collide with the soul of the northern deserts</p>
+                            <p className="text-lg md:text-3xl font-cormorant text-center text-[#ECE6D6] -ml-3 md:ml-0 md:max-w-[600px] font-[400]">Welcome to Jardín, an upscale Mexican dining destination where the vibrant flavors of the jungles in Southern Mexico collide with the soul of the northern deserts</p>
                         </div>
                         <Image
                             src="/images/home/about/about.png"
                             width={987}
                             height={1200}
                             alt="welcome"
-                            className="w-full md:w-2/4 max-h-[800px]"
+                            className="w-full md:w-3/4"
                         />
                     </div>
-                    <div className="w-full max-w-[650px] flex flex-col gap-3 justify-start items-start">
-                        <p className="text-primary-foreground font-[500] font-cormorant text-3xl">About Jardín</p>
-                        <p className="w-full max-w-[550px] text-3xl text-primary-foreground font-cormorant font-[400]">
+                    <div className="w-full md:w-3/4 flex flex-col gap-3 justify-start items-start">
+                        <p className="text-primary-foreground font-[500] font-cormorant text-3xl md:text-5xl">About Jardín</p>
+                        <p className="w-full text-3xl md:text-5xl text-primary-foreground font-cormorant font-[400] leading-[30px]">
                             Welcome to Jardín, an upscale Mexican dining destination where the vibrant flavors of the jungles in Southern Mexico collide with the soul of the northern deserts
                         </p>
                     </div>
-                    <div className="w-full max-w-[650px] flex md:justify-end">
-                        <p className="w-full max-w-[550px] text-xl text-primary-foreground font-cormorant font-[400]">
+                    <div className="w-full md:w-3/4 flex md:justify-end">
+                        <p className="w-full md:w-2/3 text-xl md:text-2xl text-primary-foreground font-cormorant font-[400]">
                             Our culinary team draws inspiration from the diverse landscapes of Mexico, blending the bold, earthy flavors of the northern deserts with the fresh, vibrant tastes of the southern jungles. Every dish is crafted with the utmost care and creativity, using the highest quality ingredients to ensure the freshest flavors.
                         </p>
                     </div>
