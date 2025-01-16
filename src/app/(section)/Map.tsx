27a21c5c43ab5bc72890ff8d8508b05fdb9f2607
jibinshan <138592544+jibinshan@users.noreply.github.com>
2025-01-16 20:31:06 +0530

    Update Map.tsx Location Change

--- src/app/(section)/Map.tsx
+++ src/app/(section)/Map.tsx
@@ -6,7 +6,7 @@ const Map = ({ }) => {
         <section id="ora" className="flex w-full items-center justify-center bg-transparent z-30">
 
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2475.8982009132355!2d-0.3649722239962183!3d51.643389371844535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876156c4ad8af4b%3A0xc70fe1d6707238c0!2sVu%20Lounge!5e0!3m2!1sen!2sin!4v1733298204427!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2475.0897846028593!2d-0.40036209999999994!3d51.658192500000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48766ac1b6bad547%3A0x6aab586ed516f809!2sJard%C3%ADn%20Mexican%20Cocina%20%26%20Tequila%20Bar!5e0!3m2!1sen!2sin!4v1737039569321!5m2!1sen!2sin"
                 style={{ border: 0 }}
                 allowFullScreen={false}
                 loading="lazy"
